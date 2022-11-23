c9ea823c4c70c78abd281c49187b9c1b4a2691af
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-11-23 16:21:34 +0100

    Bonus 1 and 2

--- js/javascript.js
+++ js/javascript.js
@@ -6,15 +6,24 @@
 
 */
 
+const frizzOutput = document.getElementById('frizz');
+const buzzOutput = document.getElementById('buzz');
+const frizzbuzzOutput = document.getElementById('frizzbuzz');
+
 for (let i = 0; i < 101; i++){
     console.log(i);
     
     if (i % 3 === 0 && i % 5 === 0){
         console.log(i + 'FrizzBuzz');
-    }
-    else if (i % 3 === 0){
+        frizzbuzzOutput.innerHTML += `<li> ${i} FrizzBuzz</li>`
+        frizzbuzzOutput.classList.add('c-red');
+    }else if (i % 3 === 0){
         console.log(i + 'frizz')
+        frizzOutput.innerHTML += `<li> ${i} Frizz</li>`
+        frizzOutput.classList.add('c-blue');
     }else if (i % 5 === 0){
         console.log(i + 'buzz');
+        buzzOutput.innerHTML += `<li> ${i} Buzz</li>`
+        buzzOutput.classList.add('c-green');
     }
 }
